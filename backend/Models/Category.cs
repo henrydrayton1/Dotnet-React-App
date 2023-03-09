3bff67a318c12f679c4c7ca0a03dc96fc4921965
Henry Drayton <henry.drayton@rmit.edu.au>
2023-03-09 16:14:43 +1100

    Working CRUD api scaffold

---
+++ backend/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace My_First_api.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+        public virtual List<Product> Products { get; set; }
+    }
+}
+
---
+++ backend/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_First_api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// builder.UseSetting("https_port", "5001");
+
+builder.Services.AddDbContext<ShopContext>(options => options.UseInMemoryDatabase("Shop"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
